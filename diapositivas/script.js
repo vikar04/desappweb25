39910ab34d5689dcf3d46dc58a6d6240769b716e
ch15 <162022970+vikar04@users.noreply.github.com>
2025-01-17 12:16:45 +0100

    a

--- diapositivas/script.js
+++ diapositivas/script.js
@@ -1,24 +1,40 @@
-let fotos= [
+let fotos = [
     "imagenes/foto1.png",
     "imagenes/foto2.png",
     "imagenes/foto3.png",
     "imagenes/foto4.jpg"
 ]
-let fotoActual = 0;
+let fotoActual = 1;
 
-function cambiardiapo(num){   
-    document.getElementById("foto").src=fotos[num];
-    fotoActual=num;
+function cambiardiapo(num) {
+    document.getElementById("foto").src = fotos[num];
+    fotoActual = num;
 }
 
-cambiardiapo(0);
 
-function anterior(){
-    document.getElementById("foto").src=fotos[fotoActual-1];
+
+function anterior() {
     fotoActual--;
+    document.getElementById("foto").src = fotos[fotoActual];
+
+    if(fotoActual==0){
+        document.getElementById("bntAnt").style.display="none";
+    }else{
+        document.getElementById("bntSig").style.display="inline";
+    }
+    
 }
 
-function siguiente(){
-    document.getElementById("foto").src=fotos[fotoActual+1];
+function siguiente() {
+   
     fotoActual++;
+    document.getElementById("foto").src = fotos[fotoActual];
+
+    if(fotoActual==fotos.length-1){
+        document.getElementById("bntSig").style.display="none";
+    }else{
+        document.getElementById("bntAnt").style.display="inline";
+    }
 }
+
+anterior();
