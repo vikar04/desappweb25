6e60d63afe091109cf4470951d0a45a76917a1f3
ch15 <162022970+vikar04@users.noreply.github.com>
2025-01-20 09:01:47 +0100

    a

--- cambioMoneda/script.js
+++ cambioMoneda/script.js
@@ -4,7 +4,7 @@ function cambiar(){
     let cantidad = document.getElementById("num").value;
     let vuelto = cantidad/conversion;
     document.getElementById("usa").src="imagenes/usa_flag.png"
-    document.querySelector("h2").innerHTML="Resultado de la conversion:"
+    document.querySelector("h2").innerHTML="Resultado de la conversion a $:"
     vuelto= vuelto.toFixed(2);
     document.getElementById("cant_convertida").innerHTML=vuelto+"$";
 }
