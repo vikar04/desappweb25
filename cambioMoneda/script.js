449a21fbd25f2445bac21b05352c7ebed80b3123
ch15 <162022970+vikar04@users.noreply.github.com>
2025-01-20 08:54:12 +0100

    a

---
+++ cambioMoneda/script.js
@@ -0,0 +1,10 @@
+let conversion = 0.97;
+
+function cambiar(){
+    let cantidad = document.getElementById("num").value;
+    let vuelto = cantidad/conversion;
+    document.getElementById("usa").src="imagenes/usa_flag.png"
+    document.querySelector("h2").innerHTML="Resultado de la conversion:"
+    vuelto= vuelto.toFixed(2);
+    document.getElementById("cant_convertida").innerHTML=vuelto+"$";
+}
