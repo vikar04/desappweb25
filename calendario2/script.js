c6a7d10f7eb5e2ff40f9e27ce1dae739cbfec667
ch15 <162022970+vikar04@users.noreply.github.com>
2025-01-21 12:23:47 +0100

    a

---
+++ calendario2/script.js
@@ -0,0 +1,22 @@
+const meses = [
+    "enero", "febrero", "marzo", "abril", "mayo", "junio",
+    "julio", "agosto", "septiembre", "octubre", "noviembre",
+    "diciembre"
+]
+
+let libreta = '';
+libreta+=`<h1>Calendario 2025</h1>`
+for (j = 0; j < 12; j++) {
+    libreta+=`<ul class="contendor"><li>L</li>
+        <li>M</li>
+        <li>X</li>
+        <li>J</li>
+        <li>V</li>
+        <li>S</li>
+        <li>D</li>`
+    for(i=1;i<=30;i++){
+        libreta+=`<li>${i}</li>`
+    }
+    libreta+=`</ul>`
+}
+document.body.innerHTML=libreta;
