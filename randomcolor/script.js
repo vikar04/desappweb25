e9d91ae0d30f991c5066ff77f848e9e18f7689cd
ch15 <162022970+vikar04@users.noreply.github.com>
2025-01-23 11:22:16 +0100

    a

--- randomcolor/script.js
+++ randomcolor/script.js
@@ -1,14 +1,40 @@
 function aleatorio(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function cambioC() {
-    let numero1 = aleatorio(0, 100);
-    let numero2 = aleatorio(0, 100);
-    let numero3 = aleatorio(0, 100);
-      
-    document.body.style.background=`rgb(${numero1},${numero2},${numero3})`;
+    /*red*/
+    let numero1 = aleatorio(0, 255);
+    /*green*/
+    let numero2 = aleatorio(0, 255);
+    /*blue*/
+    let numero3 = aleatorio(0, 255);
+
+    document.body.style.background = `rgb(${numero1},${numero2},${numero3})`;
+
+    document.getElementById("referencia").innerHTML = "red: " + numero1 + " <br>green: " + numero2 + " <br>blue: " + numero3 + ""
+}
+
+function cambioDiv() {
+
+
+    let colorDivs = document.getElementsByClassName('color');
+    let refDivs = document.getElementsByClassName('ref')
+    for (let i = 0; i < colorDivs.length; i++) {
+        /*red*/
+        let numero1 = aleatorio(0, 255);
+        /*green*/
+        let numero2 = aleatorio(0, 255);
+        /*blue*/
+        let numero3 = aleatorio(0, 255);
+
+        colorDivs[i].style.background = `rgb(${numero1}, ${numero2}, ${numero3})`;
+        refDivs[i].innerHTML="red: " + numero1 + " <br>green: " + numero2 + " <br>blue: " + numero3 + ""
     }
+    document.querySelector("li").innerHTML = "red: " + numero1 + " <br>green: " + numero2 + " <br>blue: " + numero3 + ""
+}
 
 cambioC();
+cambioDiv();
+
 setInterval(cambioC, 5000);
