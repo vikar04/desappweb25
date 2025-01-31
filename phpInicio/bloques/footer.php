cafae9a66427c25d578035dfa333e83bcc41118b
ch15 <cristiangambinomartinez@gmail.com>
2025-01-31 11:37:17 +0100

    a

---
+++ phpInicio/bloques/footer.php
@@ -0,0 +1,14 @@
+</main>
+
+<footer>
+        <p>&copy; 2025 <? echo $titulo ?> | Todos los derechos reservados</p>
+
+        <p>Este sitio web utiliza cookies para mejorar tu experiencia. <a href="#">Leer
+                más</a></p>
+
+    <?php
+    include "social.php";
+    ?>
+    </footer>
+    </body>
+    </html>
---
+++ phpInicio/index.php
@@ -0,0 +1,11 @@
+
+<?php
+//variable con el titulo web
+$titulo="Fruteria Patricia"
+include "bloques/header.php"
+?>
+
+<h1><? echo $titulo ?></h1>
+
+<?php include "bloques/footer.php"
+?>
